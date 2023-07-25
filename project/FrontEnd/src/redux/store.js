0ef9f6628765b5028f8f428ad331cecfd1d5ccda
MOHAMEDASHIF-A <125947034+MOHAMEDASHIF-A@users.noreply.github.com>
2023-07-25 14:20:13 +0530

    Delete project/FrontEnd directory

--- project/FrontEnd/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from './userslice'
-export default configureStore({
-    reducer :{
-        user:userReducer,
-        
-    }
-})
